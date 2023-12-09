9e8041d14d71c4169f9110abecaaba694206acb8
Brenden Davidson <davidson.brenden15@gmail.com>
2023-12-09 11:55:25 -0600

    Day 9 - Part 2

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -31,29 +31,47 @@ impl FromStr for OASISReport {
 }
 
 impl OASISReport {
-    fn predict_next_val(history: &[i32]) -> i32 {
+    fn compute_differences(vals: &[i32]) -> Vec<i32> {
+        vals.iter()
+            .enumerate()
+            .take(vals.len() - 1)
+            .map(|(i, val)| {
+                let next_val = &vals[i + 1];
+                next_val - val
+            })
+            .collect::<Vec<i32>>()
+    }
+
+    fn infer_next_val(history: &[i32]) -> i32 {
         if history.iter().all(|val| *val == 0) {
             // Base case
             return 0;
         }
 
-        let differences = history
-            .iter()
-            .enumerate()
-            .take(history.len() - 1)
-            .map(|(i, val)| {
-                let next_val = &history[i + 1];
-                next_val - val
-            })
-            .collect::<Vec<i32>>();
-
-        history[history.len() - 1] + OASISReport::predict_next_val(&differences)
+        history[history.len() - 1]
+            + OASISReport::infer_next_val(&OASISReport::compute_differences(&history))
     }
 
-    fn sum_all_predicted_vals(&self) -> i32 {
+    fn sum_all_next_vals(&self) -> i32 {
         self.histories
             .iter()
-            .map(|history| OASISReport::predict_next_val(&history))
+            .map(|history| OASISReport::infer_next_val(&history))
+            .sum()
+    }
+
+    fn infer_prev_val(history: &[i32]) -> i32 {
+        if history.iter().all(|val| *val == 0) {
+            // Base case
+            return 0;
+        }
+
+        history[0] - OASISReport::infer_prev_val(&OASISReport::compute_differences(&history))
+    }
+
+    fn sum_all_prev_vals(&self) -> i32 {
+        self.histories
+            .iter()
+            .map(|history| OASISReport::infer_prev_val(&history))
             .sum()
     }
 }
@@ -64,9 +82,11 @@ fn main() {
         .parse::<OASISReport>()
         .expect("failed to parse input");
 
-    let predicted_vals_sum = oasis_report.sum_all_predicted_vals();
+    let next_vals_sum = oasis_report.sum_all_next_vals();
+    println!("Part 1 result: {next_vals_sum}");
 
-    println!("Part 1 result: {predicted_vals_sum}");
+    let prev_vals_sum = oasis_report.sum_all_prev_vals();
+    println!("Part 2 result: {prev_vals_sum}");
 }
 
 #[cfg(test)]
@@ -93,13 +113,21 @@ mod test {
     #[test]
     fn predict_next_val() {
         let test_data = [
-            (vec![0, 3, 6, 9, 12, 15], 18),
-            (vec![1, 3, 6, 10, 15, 21], 28),
-            (vec![10, 13, 16, 21, 30, 45], 68),
+            ([0, 3, 6, 9, 12, 15], 18),
+            ([1, 3, 6, 10, 15, 21], 28),
+            ([10, 13, 16, 21, 30, 45], 68),
         ];
 
         for (history, expected) in test_data {
-            assert_eq!(OASISReport::predict_next_val(&history), expected);
+            assert_eq!(OASISReport::infer_next_val(&history), expected);
         }
+    }
+
+    #[test]
+    fn predict_previous_val() {
+        let history = [10, 13, 16, 21, 30, 45];
+        let expected = 5;
+
+        assert_eq!(OASISReport::infer_prev_val(&history), expected);
     }
 }
